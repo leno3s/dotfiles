a7527bc259392c821e956a2f966758a40f477869
leno3s <3so.d.na@gmail.com>
2019-11-04 03:21:27 +0900

    executableじゃない

--- dein_installer.sh
+++ dein_installer.sh
@@ -1,4 +1,4 @@
 cd /tmp
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ./dein_installer.sh
-./dein_installer.sh ~/.vim/dein/
+sh ./dein_installer.sh ~/.vim/dein/
 rm ./dein_installer.sh
